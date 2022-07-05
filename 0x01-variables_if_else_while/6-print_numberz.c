4f370a08b8b0ee4d7b38292d8a97c2486c6fb3d0
owusu <simboahen@gmail.com>
2022-07-04 17:57:54 -0700

    variable file 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main -> assign a random number to the variable n each time it is executed
 * and print the last digit of the number stored in the variable n
@@ -7,13 +8,14 @@
 int main(void)
 {
 	int ch;
-	for (ch = 48; ch <= 57; ch++)
 
+	for (ch = 48; ch <= 57; ch++)
 	{
-	putchar(ch);
+
+		putchar(ch);
 	}
 
 	putchar(10); /* this is an ascii code for new line*/
+
 	return (0);
 }
-
